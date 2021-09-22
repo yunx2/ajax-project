40c47aefceab98c7eb79f8049b815d0960007e7d
Yunyun Liu <celeph4is@gmail.com>
2021-09-22 11:51:07 -0700

    remove console logs and extracss

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-// const baseUrl = 'https://acnhapi.com/v1/'
 const $find = document.getElementById('find');
 const $resultView = document.getElementById('result-view');
-// const $select = document.getElementById('select-creature')
 const $form = document.getElementById('form');
-// const $findView = document.getElementById('find-view')
 const $message = document.getElementById('message');
 const $north = document.getElementById('north');
 const $south = document.getElementById('south');
@@ -15,9 +12,7 @@ function checkAvailability(hemisphere) {
     return true;
   }
   const currentMonth = new Date().getMonth() + 1;
-  console.log('month:', currentMonth)
   const availability = data.response.availability[`month-array-${hemisphere}`];
-  console.log('availability:', availability)
   return availability.includes(currentMonth);
 }
 
@@ -32,44 +27,37 @@ function displayAvailable(availability, node) {
 const request = new XMLHttpRequest();
 request.responseType = 'json';
 request.addEventListener('load', e => {
-  // inside this event listener, guaranteed to have response data
+  // inside this event listener, guaranteed to have a response
   data.response = request.response;
-  // console.log('response:', request.response);
   if (data.response) {
     // handle response
-    // console.log('yes there\'s data')
-    // console.log('response:', data.response)
     $message.textContent = null;
     const creatureName = data.response['file-name'];
-    $resultImg.setAttribute('src', data.response.image_uri);
+    $resultImg.setAttribute('src', data.response.icon_uri);
     $resultImg.setAttribute('alt', creatureName);
     $resultName.textContent = creatureName.toUpperCase();
 
     const availableNorth = checkAvailability('northern');
     const availableSouth = checkAvailability('southern');
-    // console.log('availableNorth:', availableNorth)
-    // console.log('availableSouth:', availableSouth)
     displayAvailable(availableNorth, $north);
     displayAvailable(availableSouth, $south);
     $resultView.classList.remove('hidden');
     $resultView.scrollIntoView();
   } else {
     // show not found message
+    $resultView.classList.add('hidden');
     $message.textContent = `No information about '${data.creature}'. Try again.`;
-    // $message.classList.remove('hidden');
   }
+  $form.reset();
 });
 
 function handleFind(e) {
   const $selectControl = $form.elements.select;
   const $textControl = $form.elements['text-input'];
   const type = $selectControl.value;
-  data.creature = $textControl.value;
-  request.open('GET', `https://acnhapi.com/v1/${type}/${$textControl.value}`);
+  data.creature = $textControl.value.toLowerCase();
+  request.open('GET', `https://acnhapi.com/v1/${type}/${data.creature}`);
   request.send();
-//   console.log('value:', $selectControl.value)
-//   console.log('value:', $textControl.value)
-  // $form.reset();
 }
 
 $find.addEventListener('click', handleFind);
