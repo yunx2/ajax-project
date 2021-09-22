e9db923c7b8edbd45a32b9e44659451b94eca1ea
Yunyun Liu <celeph4is@gmail.com>
2021-09-22 10:50:16 -0700

    add function to check for creature availability

--- js/main.js
+++ js/main.js
@@ -5,15 +5,32 @@ const $find = document.getElementById('find');
 const $form = document.getElementById('form');
 // const $findView = document.getElementById('find-view')
 const $message = document.getElementById('message');
+function checkAvailability(hemisphere) {
+  if (data.response.availability.isAllYear) {
+    return true;
+  }
+  const currentMonth = new Date().getMonth() + 1;
+  console.log('month:', currentMonth)
+  const availability = data.response.availability[`month-array-${hemisphere}`];
+  console.log('availability:', availability)
+  return availability.includes(currentMonth);
+
+}
 
 const request = new XMLHttpRequest();
 request.responseType = 'json';
 request.addEventListener('load', e => {
   // inside this event listener, guaranteed to have response data
   data.response = request.response;
-  console.log('response:', request.response);
+  // console.log('response:', request.response);
   if (data.response) {
     // handle response
+    // console.log('yes there\'s data')
+    // console.log('response:', data.response)
+    const availableNorth = checkAvailability('northern');
+    const availableSouth = checkAvailability('southern');
+    console.log('availableNorth:', availableNorth)
+    console.log('availableSouth:', availableSouth)
 
   } else {
     // show not found message
