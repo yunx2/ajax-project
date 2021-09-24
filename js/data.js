8b93c73149442c8107f5a53967570218ef9624c8
Yunyun Liu <celeph4is@gmail.com>
2021-09-24 10:39:54 -0700

    change view calls setCatchList before switch to list view

--- js/data.js
+++ js/data.js
@@ -9,6 +9,80 @@ let data = {
   nextId: 1
 };
 
+function createCatchItem(entry) {
+  const $li = document.createElement('li');
+  $li.classList.add('catch-item');
+  const $topRow = document.createElement('div');
+  $topRow.classList.add('row');
+  $topRow.classList.add('top-row');
+
+  const $leftCol = document.createElement('div');
+  const $img = document.createElement('img');
+  $img.setAttribute('src', entry.creatureData.icon_uri);
+  $img.setAttribute('alt', entry.creatureName);
+  $leftCol.classList.add('column-third');
+  $leftCol.classList.add('col-left');
+  $leftCol.append($img);
+
+  const $midCol = document.createElement('div');
+  $midCol.classList.add('column-third');
+  $midCol.classList.add('col-mid');
+  const $name = document.createElement('p');
+  $name.textContent = entry.creatureName;
+  $name.classList.add('list-item-title');
+  $midCol.append($name);
+
+  const $rightCol = document.createElement('div');
+  const $check = document.createElement('input');
+  $check.setAttribute('type', 'checkbox');
+  const $caught = document.createElement('span');
+  $caught.className = 'caught-check';
+  $caught.textContent = 'Caught!';
+  const $label = document.createElement('label');
+  $label.append($caught, $check);
+  $rightCol.classList.add('column-third');
+  $rightCol.classList.add('col-right');
+  $rightCol.append($label);
+  $topRow.append($leftCol, $midCol, $rightCol);
+
+  const $bottomRow = document.createElement('div');
+  $bottomRow.className = 'row';
+  const $commentsRow = document.createElement('div');
+  $commentsRow.classList.add('column-full');
+  $commentsRow.classList.add('comments-row');
+  $bottomRow.append($commentsRow);
+  const $comments = document.createElement('span');
+  $comments.textContent = 'Comment:';
+  const $button = document.createElement('button');
+  $button.setAttribute('type', 'button');
+  $button.className = 'btn-edit';
+  $button.innerHTML = '<i class="fas fa-pen"></i>';
+  $commentsRow.append($comments, $button);
+  $li.append($topRow, $bottomRow);
+  // console.log($li);
+  return $li;
+}
+
+function setCatchList() {
+  if (data.catchList.length === 0) {
+    document.getElementById('message-list').classList.remove('hidden');
+  } else {
+    // remove any items that are already set
+    document.getElementById('message-list').classList.add('hidden');
+    const listItems = document.getElementsByClassName('catch-item');
+    for (let i = 0; i < listItems.length; i++) {
+      const $item = listItems[i];
+      $item.remove();
+    }
+    // set items again from catchlist
+    data.catchList.forEach(current => {
+      const $item = createCatchItem(current);
+      // console.log($item)
+      $catchList.append($item);
+    });
+  }
+}
+
 const viewsList = document.querySelectorAll('[data-view]');
 
 function changeView(view) {
