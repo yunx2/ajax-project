1e63a1a39515da0b2d4cad1a1e0654f80f9eba27
Yunyun Liu <celeph4is@gmail.com>
2021-09-26 22:42:22 -0700

    remove commented out code

--- js/main.js
+++ js/main.js
@@ -78,28 +78,10 @@ $editButtonsContainer.addEventListener('click', e => {
   $catchList.scrollIntoView();
 });
 
-// const $confirmDelete = document.querySelector('dialog');
-// const $undo = document.getElementById('btn-undo');
-// const $delete = document.getElementById('btn-delete');
-
-// $undo.addEventListener('click', e => {
-
-//   $confirmDelete.close();
-// })
-
 function handleCheck(e) {
   const $toDelete = document.querySelector(`[data-id='${e.target.id}']`);
-  // data.editing = e.target.getAttribute('data-id');
-  // console.log(data.editing)
-  // document.getElementById('confirm').textContent = `Remove ${$toDelete.getAttribute('data-creature')}?`;
-  // $confirmDelete.showModal();
   $toDelete.remove();
   data.catchList = data.catchList.filter(item => item.id != e.target.id);
-  // console.log('list after delele:', data.catchList)
-  // $toDelete.classList.add('hidden');
-  // const $confirm = document.getElementById('confirm');
-  // $confirmDelete.showModal();
-  // console.log('$toDelete:', $toDelete);
 }
 
 $catchList.addEventListener('input', e => handleCheck(e));
