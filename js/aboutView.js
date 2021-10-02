88f0c97d14e773d020946ed6058055411920eda4
Yunyun Liu <celeph4is@gmail.com>
2021-10-02 13:38:33 -0700

    clicking on a serach term takes user to find view with the search prepopulated

--- js/aboutView.js
+++ js/aboutView.js
@@ -3,3 +3,15 @@ function setSearch(type, creature) {
   $textControl.value = creature;
   changeView('find');
 }
+
+const $aboutList = document.querySelector('.about-list');
+
+$aboutList.addEventListener('click', e => {
+  // console.log('target', e.target)
+  if (e.target.tagName !== 'BUTTON') {
+    return;
+  }
+  const type = e.target.getAttribute('data-type');
+  // console.log('target id', e.target.id);
+  setSearch(type, e.target.id)
+});
